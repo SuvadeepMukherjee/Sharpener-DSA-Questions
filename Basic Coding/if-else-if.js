fb5c655046c825a811c224a636bf2e8a7da200d9
Suvadeep <suvadeepm0709@gmail.com>
2024-06-28 22:10:37 +0530

    Added if-else-if

---
+++ Basic Coding/if-else-if.js
@@ -0,0 +1,10 @@
+let d = 700;
+if (d >= 0 && d <= 100) {
+  console.log("cost is 5.00");
+} else if (d > 100 && d <= 500) {
+  console.log("cost is 8.00");
+} else if (d > 500 && d <= 1000) {
+  console.log("cost is 10.00");
+} else if (d > 1000) {
+  console.log("cost is 12.00");
+}
