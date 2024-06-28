bdb01e8e56793619b39a13bfd071a4dd1a8e21b9
Suvadeep <suvadeepm0709@gmail.com>
2024-06-28 22:23:11 +0530

    switch1

---
+++ Basic Coding/switch1.js
@@ -0,0 +1,29 @@
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("day is monday");
+    break;
+
+  case 2:
+    console.log("day is tuesday");
+    break;
+  case 3:
+    console.log("day is wednesday ");
+    break;
+  case 4:
+    console.log("day is thursday");
+    break;
+  case 5:
+    console.log("day is friday");
+    break;
+  case 6:
+    console.log("day is saturday");
+    break;
+  case 7:
+    console.log("day is sunday");
+    break;
+  default:
+    console.log("invalid day");
+    break;
+}
