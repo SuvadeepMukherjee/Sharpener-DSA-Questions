037f439796a246db45e250f50645038d0b5d48ca
Suvadeep <suvadeepm0709@gmail.com>
2024-06-29 19:13:00 +0530

    Insertion Sort

---
+++ Funadmentals of Coding And Linear DSA/Arrays/insertionSort.js
@@ -0,0 +1,14 @@
+function sort_array(arr, length) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+console.log(sort_array([4, 3, 2, 5, 1]));
