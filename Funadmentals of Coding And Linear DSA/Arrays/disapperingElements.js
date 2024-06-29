e0beb20e2bf8bd11eaf597e14d5c7f1de3a8e8b0
Suvadeep <suvadeepm0709@gmail.com>
2024-06-29 18:55:39 +0530

    Disappearing elements

---
+++ Funadmentals of Coding And Linear DSA/Arrays/disapperingElements.js
@@ -0,0 +1,16 @@
+var findDisappearedNumbers = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let j = Math.abs(nums[i]) - 1;
+    nums[j] = Math.abs(nums[j]) * -1;
+  }
+
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+
+  return result;
+};
