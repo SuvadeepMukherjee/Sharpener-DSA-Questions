30c97e15ccb1be2a384ccced6087643d0aecf044
Suvadeep <suvadeepm0709@gmail.com>
2024-06-29 05:43:30 +0530

    Maximum in an array

---
+++ Funadmentals of Coding And Linear DSA/Arrays/maximumInAnArray.js
@@ -0,0 +1,10 @@
+function find_maximum(arr, length) {
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(find_maximum([5, 4, 7, 2, 6]));
