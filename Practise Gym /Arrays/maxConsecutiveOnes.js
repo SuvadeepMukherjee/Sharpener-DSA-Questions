9daca51299c7c660c81e11feeaf39f50f75fdd9b
Suvadeep <suvadeepm0709@gmail.com>
2024-06-29 20:43:15 +0530

    Max Xonsecutive Ones

---
+++ Practise Gym /Arrays/maxConsecutiveOnes.js
@@ -0,0 +1,15 @@
+var countOnes = function (nums) {
+  let currentCount = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      currentCount++;
+      maxCount = Math.max(maxCount, currentCount);
+    } else {
+      currentCount = 0;
+    }
+  }
+
+  return maxCount;
+};
