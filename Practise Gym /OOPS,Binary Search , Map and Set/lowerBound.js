66fe1aa3bceafb5e9af555c48b20eb70c0509c1d
Suvadeep <suvadeepm0709@gmail.com>
2024-06-30 03:10:18 +0530

    Lower bound

---
+++ Practise Gym /OOPS,Binary Search , Map and Set/lowerBound.js
@@ -0,0 +1,16 @@
+var lowerBound = function (nums, target) {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+};
