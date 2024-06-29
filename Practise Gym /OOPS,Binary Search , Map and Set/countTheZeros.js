dd79c14dbf3d3fa6aa986e4b82fdd6ff1a92c673
Suvadeep <suvadeepm0709@gmail.com>
2024-06-30 03:07:40 +0530

    Count the Zeroes

---
+++ Practise Gym /OOPS,Binary Search , Map and Set/countTheZeros.js
@@ -0,0 +1,16 @@
+var countZeros = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === 1) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return nums.length - left;
+};
